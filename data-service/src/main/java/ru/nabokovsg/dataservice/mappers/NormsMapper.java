0aa17eb2195ebb446018cf74ac6d0768ae1d3570
Nabokov79 <nabokovsg@yandex.ru>
2023-10-07 18:03:04 +0300

    add mapper

--- data-service/src/main/java/ru/nabokovsg/dataservice/mappers/NormsMapper.java
+++ data-service/src/main/java/ru/nabokovsg/dataservice/mappers/NormsMapper.java
@@ -2,7 +2,7 @@ package ru.nabokovsg.dataservice.mappers;
 
 import org.mapstruct.Mapper;
 import ru.nabokovsg.dataservice.dto.norms.NewNormsDto;
-import ru.nabokovsg.dataservice.dto.norms.NormSearchParameters;
+import ru.nabokovsg.dataservice.dto.norms.NormSearchParametersDto;
 import ru.nabokovsg.dataservice.dto.norms.NormsDto;
 import ru.nabokovsg.dataservice.dto.norms.UpdateNormsDto;
 import ru.nabokovsg.dataservice.models.Norm;
@@ -18,5 +18,5 @@ public interface NormsMapper {
 
     List<NormsDto> mapToNormsDto(List<Norm> norms);
 
-    NormSearchParameters mapToNormSearchParameters(NewNormsDto normDto);
+    NormSearchParametersDto mapToNormSearchParameters(NewNormsDto normDto);
 }
