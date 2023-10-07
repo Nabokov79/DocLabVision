7ff4867674be4310a5aa95d77c441759b2769359
Nabokov79 <nabokovsg@yandex.ru>
2023-10-07 17:28:51 +0300

    add model

---
+++ data-service/src/main/java/ru/nabokovsg/dataservice/models/Address.java
@@ -0,0 +1,43 @@
+package ru.nabokovsg.dataservice.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "addresses")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "city")
+    private String city;
+    @Column(name = "street")
+    private String street;
+    @Column(name = "house_number")
+    private Integer houseNumber;
+    @Column(name = "building_number")
+    private Integer buildingNumber;
+    @Column(name = "letter")
+    private String letter;
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "id=" + id +
+                ", city=" + city +
+                ", street='" + street + '\'' +
+                ", houseNumber=" + houseNumber +
+                ", buildingNumber=" + buildingNumber +
+                ", letter='" + letter + '\'' +
+                '}';
+    }
+}
