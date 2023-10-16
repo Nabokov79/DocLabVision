3f2bad9eaafe45300d37781d719a5d2048786db5
NabokovSG <nabokovsg@yandex.ru>
2023-10-16 15:17:03 +0300

    add const

---
+++ data-service/src/main/java/ru/nabokovsg/dataservice/models/DocumentType.java
@@ -0,0 +1,18 @@
+package ru.nabokovsg.dataservice.models;
+
+import java.util.Optional;
+
+public enum DocumentType {
+
+    REPORT,
+    PROTOCOL;
+
+    public static Optional<DocumentType> from(String documentType) {
+        for (DocumentType type : values()) {
+            if (type.name().equalsIgnoreCase(documentType)) {
+                return Optional.of(type);
+            }
+        }
+        return Optional.empty();
+    }
+}
