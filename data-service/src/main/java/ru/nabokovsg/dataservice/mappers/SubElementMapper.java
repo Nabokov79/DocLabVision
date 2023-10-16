9c25adb6c7f7dc72df1dcc894fe22072251d4f85
NabokovSG <nabokovsg@yandex.ru>
2023-10-16 15:24:48 +0300

    without changes

--- data-service/src/main/java/ru/nabokovsg/dataservice/mappers/SubElementMapper.java
+++ data-service/src/main/java/ru/nabokovsg/dataservice/mappers/SubElementMapper.java
@@ -5,14 +5,10 @@ import ru.nabokovsg.dataservice.dto.subElement.NewSubElementDto;
 import ru.nabokovsg.dataservice.dto.subElement.UpdateSubElementDto;
 import ru.nabokovsg.dataservice.models.SubElement;
 
-import java.util.List;
-
 @Mapper(componentModel = "spring")
 public interface SubElementMapper {
 
     SubElement mapToNewSubElement(NewSubElementDto subElementDto);
 
-    List<SubElement> mapToNewSubElements(List<NewSubElementDto> subElementsDto);
-
-    List<SubElement> mapToUpdateSubElements(List<UpdateSubElementDto> subElementsDto);
+   SubElement mapToUpdateSubElements(UpdateSubElementDto subElementDto);
 }
