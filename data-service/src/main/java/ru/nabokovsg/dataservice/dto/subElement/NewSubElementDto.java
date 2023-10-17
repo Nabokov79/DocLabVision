fc316d5e8d4332160ff5097087fcef3492109810
Nabokov79 <nabokovsg@yandex.ru>
2023-10-17 20:09:12 +0300

    add NoArgsConstructor

--- data-service/src/main/java/ru/nabokovsg/dataservice/dto/subElement/NewSubElementDto.java
+++ data-service/src/main/java/ru/nabokovsg/dataservice/dto/subElement/NewSubElementDto.java
@@ -3,6 +3,7 @@ package ru.nabokovsg.dataservice.dto.subElement;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
@@ -10,6 +11,7 @@ import javax.validation.constraints.NotBlank;
 @Setter
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 @Schema(description = "Данные о подэлементе элемента объекта")
 public class NewSubElementDto {
 
