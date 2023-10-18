bbe97962ac7b846d22092c41a8d14b62424eb6c7
NabokovSG <nabokovsg@yandex.ru>
2023-10-18 16:57:52 +0300

    add model

--- templ-service/src/main/java/ru/nabokovsg/temlservice/models/ProtocolTemplate.java
+++ templ-service/src/main/java/ru/nabokovsg/temlservice/models/ProtocolTemplate.java
@@ -1,9 +1,6 @@
 package ru.nabokovsg.temlservice.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.List;
@@ -19,10 +16,28 @@ public class ProtocolTemplate {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @OneToOne
+    @JoinColumn(name = "left_title_id", referencedColumnName = "id")
     private PageTitle leftTitle;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "protocol_templates_table_subsections_templates",
+            joinColumns = {@JoinColumn(name = "protocol_template_id")},
+            inverseJoinColumns = {@JoinColumn(name = "subsections_template_id")})
+    @ToString.Exclude
     private List<SubsectionTemplate> subsectionsTemplate;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "protocol_templates_table_header_templates",
+            joinColumns = {@JoinColumn(name = "protocol_template_id")},
+            inverseJoinColumns = {@JoinColumn(name = "header_template_id")})
+    @ToString.Exclude
     private List<TableHeaderTemplate> tablesHeaderTemplate;
-    private List<Recommendation> recommendations;
-    private ConclusionTemplate conclusionsTemplate;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "protocol_templates_table_appendices",
+            joinColumns = {@JoinColumn(name = "protocol_template_id")},
+            inverseJoinColumns = {@JoinColumn(name = "appendices_id")})
+    @ToString.Exclude
     private List<Appendices> appendices;
 }
--- templ-service/src/main/java/ru/nabokovsg/temlservice/models/ReportTemplate.java
+++ templ-service/src/main/java/ru/nabokovsg/temlservice/models/ReportTemplate.java
@@ -1,21 +1,40 @@
 package ru.nabokovsg.temlservice.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "report_templates")
+@Table(name = "reports_templates")
 public class ReportTemplate {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @OneToOne
+    @JoinColumn(name = "page_title_id", referencedColumnName = "id")
+    private PageTitle pageTitle;
+    @Column(name = "document_name")
+    private String documentName;
+    @Column(name = "document_title")
+    private String documentTitle;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "reports_templates_table_subsection_templates",
+            joinColumns = {@JoinColumn(name = "reports_template_id")},
+            inverseJoinColumns = {@JoinColumn(name = "subsection_template_id")})
+    @ToString.Exclude
+    private List<SubsectionTemplate> subsectionTemplates;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "report_templates_table_appendices",
+            joinColumns = {@JoinColumn(name = "report_template_id")},
+            inverseJoinColumns = {@JoinColumn(name = "appendices_id")})
+    @ToString.Exclude
+    private List<Appendices> appendices;
 }
