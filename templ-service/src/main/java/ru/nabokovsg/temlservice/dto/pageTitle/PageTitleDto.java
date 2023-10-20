668b1408a2894b0e1b4f9f7062e366b082dafe2b
NabokovSG <nabokovsg@yandex.ru>
2023-10-20 15:27:45 +0300

    add dto

---
+++ templ-service/src/main/java/ru/nabokovsg/temlservice/dto/pageTitle/PageTitleDto.java
@@ -0,0 +1,34 @@
+package ru.nabokovsg.temlservice.dto.pageTitle;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@Schema(description = "Данные титульного листа отчета, протокола, заключения")
+public class PageTitleDto {
+
+    @Schema(description = "Индентификатор")
+    private Long id;
+    @Schema(description = "Организация")
+    private String organization;
+    @Schema(description = "Лицензия организации")
+    private String organizationLicense;
+    @Schema(description = "Контактные данные организации")
+    private String organizationRequisites;
+    @Schema(description = "Филиал организации")
+    private String branch;
+    @Schema(description = "Контактные данные филиала организации")
+    private String branchRequisites;
+    @Schema(description = "Лицензия филиала организации")
+    private String licenseBranch;
+    @Schema(description = "Подразделение филиала организации")
+    private String department;
+    @Schema(description = "Контактные данные подразделения филиала организации")
+    private String departmentRequisites;
+    @Schema(description = "Лицензия подразделения филиала организации")
+    private String departmentLicense;
+}
