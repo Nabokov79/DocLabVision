a32dc09e5dc70a96fb6ef62fd7ee49f66226691f
Nabokov79 <nabokovsg@yandex.ru>
2023-10-22 18:58:03 +0300

    delete

--- templ-service/src/main/java/ru/nabokovsg/temlservice/services/PageTitleService.java
+++
@@ -1,9 +0,0 @@
-package ru.nabokovsg.temlservice.services;
-
-import ru.nabokovsg.temlservice.dto.pageTitle.NewPageTitleDto;
-import ru.nabokovsg.temlservice.models.PageTitle;
-
-public interface PageTitleService {
-
-    PageTitle save(NewPageTitleDto pageTitleDto);
-}
--- templ-service/src/main/java/ru/nabokovsg/temlservice/services/TemplateService.java
+++
@@ -1,9 +0,0 @@
-package ru.nabokovsg.temlservice.services;
-
-import ru.nabokovsg.temlservice.dto.templates.DocumentTemplateDto;
-import ru.nabokovsg.temlservice.dto.templates.NewTemplateDto;
-
-public interface TemplateService {
-
-    DocumentTemplateDto save(NewTemplateDto reportTemplateDto);
-}
