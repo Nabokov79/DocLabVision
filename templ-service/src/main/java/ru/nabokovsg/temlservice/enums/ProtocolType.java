47bf9634274ac7f4d1dfc2fe9496ad06eb72e8c1
NabokovSG <nabokovsg@yandex.ru>
2023-10-27 16:02:34 +0300

    add enum

---
+++ templ-service/src/main/java/ru/nabokovsg/temlservice/enums/ProtocolType.java
@@ -0,0 +1,21 @@
+package ru.nabokovsg.temlservice.enums;
+
+import java.util.Optional;
+
+public enum ProtocolType {
+
+    VISUAL_MEASURING_CONTROL,
+    ULTRASONIC_MEASURING_CONTROL,
+    GEODESIC_MEASURING,
+    ULTRASONIC_CONTROL,
+    HARDNESS_MEASURING;
+
+    public static Optional<ProtocolType> from(String protocolType) {
+        for (ProtocolType type : values()) {
+            if (type.name().equalsIgnoreCase(protocolType)) {
+                return Optional.of(type);
+            }
+        }
+        return Optional.empty();
+    }
+}
