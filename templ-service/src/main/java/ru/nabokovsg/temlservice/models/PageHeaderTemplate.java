c23c52117157a17901a6bf1eb486e20c4a002672
NabokovSG <nabokovsg@yandex.ru>
2023-10-27 16:02:19 +0300

    add model

---
+++ templ-service/src/main/java/ru/nabokovsg/temlservice/models/PageHeaderTemplate.java
@@ -0,0 +1,39 @@
+package ru.nabokovsg.temlservice.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "page_header_templates")
+public class PageHeaderTemplate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "organization")
+    private String organization;
+    @Column(name = "organization_license")
+    private String organizationLicense;
+    @Column(name = "requisites_organization")
+    private String organizationRequisites;
+    @Column(name = "branch")
+    private String branch;
+    @Column(name = "branch_requisites")
+    private String branchRequisites;
+    @Column(name = "license_branch")
+    private String licenseBranch;
+    @Column(name = "department")
+    private String department;
+    @Column(name = "department_requisites")
+    private String departmentRequisites;
+    @Column(name = "department_license")
+    private String departmentLicense;
+}
