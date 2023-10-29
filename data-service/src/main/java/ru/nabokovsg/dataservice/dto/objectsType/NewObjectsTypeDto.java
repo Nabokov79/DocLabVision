dc2f18bc810c81d42a084f603f009f5440d99517
Nabokov79 <nabokovsg@yandex.ru>
2023-10-29 19:00:06 +0300

    add toString(), equals, hashCode

--- data-service/src/main/java/ru/nabokovsg/dataservice/dto/objectsType/NewObjectsTypeDto.java
+++ data-service/src/main/java/ru/nabokovsg/dataservice/dto/objectsType/NewObjectsTypeDto.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import javax.validation.constraints.NotBlank;
+import java.util.Objects;
 
 @Setter
 @Getter
@@ -19,4 +20,32 @@ public class NewObjectsTypeDto {
     private String orientation;
     @Schema(description = "Объем объекта")
     private Integer volume;
+    @Schema(description = "Модель объекта")
+    private String model;
+
+    @Override
+    public String toString() {
+        return "NewObjectsTypeDto{" +
+                "objectName='" + objectName + '\'' +
+                ", orientation='" + orientation + '\'' +
+                ", volume=" + volume +
+                ", model='" + model + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        NewObjectsTypeDto that = (NewObjectsTypeDto) o;
+        return Objects.equals(objectName, that.objectName)
+            && Objects.equals(orientation, that.orientation)
+            && Objects.equals(volume, that.volume)
+            && Objects.equals(model, that.model);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(objectName, orientation, volume, model);
+    }
 }
