65e4f79b057b5ed2a39e4e8908225f663aafe912
Nabokov79 <nabokovsg@yandex.ru>
2023-10-29 19:12:14 +0300

    no changes

--- data-service/src/main/java/ru/nabokovsg/dataservice/dto/dataPassportOfObject/UpdateDataPassportOfObjectDto.java
+++ data-service/src/main/java/ru/nabokovsg/dataservice/dto/dataPassportOfObject/UpdateDataPassportOfObjectDto.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
@@ -26,6 +25,5 @@ public class UpdateDataPassportOfObjectDto {
     private Long templateId;
     @Schema(description = "Значение данных паспорта объекта")
     @NotBlank(message = "meaning should not be blank")
-    @Min(3)
     private String meaning;
 }
