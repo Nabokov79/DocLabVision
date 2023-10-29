c8a6adef2ad179d6715081f1000b19d6bc4fb812
Nabokov79 <nabokovsg@yandex.ru>
2023-10-29 19:12:21 +0300

    no changes

--- data-service/src/main/java/ru/nabokovsg/dataservice/dto/measuringTool/UpdateMeasuringToolDto.java
+++ data-service/src/main/java/ru/nabokovsg/dataservice/dto/measuringTool/UpdateMeasuringToolDto.java
@@ -27,8 +27,7 @@ public class UpdateMeasuringToolDto {
     @NotBlank(message = "model should not be blank")
     private String model;
     @Schema(description = "Заводской номер")
-    @NotNull(message = "work number should not be blank")
-    @Positive(message = "work number must be positive")
+    @NotBlank(message = "work number should not be blank")
     private String workNumber;
     @Schema(description = "Назначение")
     @NotBlank(message = "purpose should not be blank")
@@ -70,7 +69,6 @@ public class UpdateMeasuringToolDto {
     @NotBlank(message = "registry should not be blank")
     private String registry;
     @Schema(description = "Примечание")
-    @NotBlank(message = "note should not be blank")
     private String note;
     @Schema(description = "Вид контроля")
     private String controlType;
