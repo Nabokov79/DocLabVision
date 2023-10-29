ea80c72fd110a9236f8fdc3653b42e4a3e69526d
Nabokov79 <nabokovsg@yandex.ru>
2023-10-29 18:57:47 +0300

    no changes

--- data-service/src/main/java/ru/nabokovsg/dataservice/repository/ElementRepository.java
+++ data-service/src/main/java/ru/nabokovsg/dataservice/repository/ElementRepository.java
@@ -4,13 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import ru.nabokovsg.dataservice.models.Element;
-import ru.nabokovsg.dataservice.models.ObjectsType;
-
 import java.util.List;
 import java.util.Set;
 
 public interface ElementRepository extends JpaRepository<Element, Long> {
 
-    @Query("select e from Element e where e.objectsType in :objectsType")
-    Set<Element> findAllByObjectsType(@Param("objectsType")List<ObjectsType> objectsType);
+    @Query("select e from Element e where e.objectsTypeId in :ids")
+    Set<Element> findAllByObjectsTypeId(@Param("ids") List<Long> ids);
 }
