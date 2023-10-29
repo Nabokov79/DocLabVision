bc7dbba1b28ab8d9c360f1dc30f4853d64a13d7e
Nabokov79 <nabokovsg@yandex.ru>
2023-10-29 19:05:31 +0300

    add Valid in methods

--- data-service/src/main/java/ru/nabokovsg/dataservice/controllers/ObjectsTypeController.java
+++ data-service/src/main/java/ru/nabokovsg/dataservice/controllers/ObjectsTypeController.java
@@ -10,9 +10,13 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.nabokovsg.dataservice.dto.objectsType.NewObjectsTypeDto;
 import ru.nabokovsg.dataservice.dto.objectsType.ObjectsTypeDto;
+import ru.nabokovsg.dataservice.dto.objectsType.ShortObjectsTypeDto;
 import ru.nabokovsg.dataservice.dto.objectsType.UpdateObjectsTypeDto;
 import ru.nabokovsg.dataservice.services.ObjectsTypeService;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import java.util.List;
 
 @RestController
@@ -30,28 +34,30 @@ public class ObjectsTypeController {
 
     @Operation(summary = "Добавление данных типа объекта")
     @PostMapping
-    public ResponseEntity<List<ObjectsTypeDto>> save(@RequestBody
+    public ResponseEntity<List<ShortObjectsTypeDto>> save(
+                                      @RequestBody @Valid
                                       @Parameter(description = "Тип объекта") List<NewObjectsTypeDto> objectsTypeDto ) {
         return ResponseEntity.ok().body(service.save(objectsTypeDto));
     }
 
     @Operation(summary = "Изменение данных типа объекта")
     @PatchMapping
-    public ResponseEntity<List<ObjectsTypeDto>> update(@RequestBody
+    public ResponseEntity<List<ShortObjectsTypeDto>> update(
+                                    @RequestBody @Valid
                                     @Parameter(description = "Тип объекта") List<UpdateObjectsTypeDto> objectsTypeDto) {
         return ResponseEntity.ok().body(service.update(objectsTypeDto));
     }
 
     @Operation(summary = "Удаление типа объекта")
     @GetMapping("/{id}")
-    public ResponseEntity<ObjectsTypeDto> get(@PathVariable
-                                         @Parameter(description = "Индентификатор типа объекта") Long id) {
+    public ResponseEntity<ObjectsTypeDto> get(@PathVariable @NotNull @Positive
+                                              @Parameter(description = "Индентификатор типа объекта") Long id) {
         return ResponseEntity.ok().body(service.get(id));
     }
 
     @Operation(summary = "Удаление типа объекта")
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> delete(@PathVariable
+    public ResponseEntity<String> delete(@PathVariable @NotNull @Positive
                                          @Parameter(description = "Индентификатор типа объекта") Long id) {
         service.delete(id);
         return ResponseEntity.ok("Тип объекта удален.");
