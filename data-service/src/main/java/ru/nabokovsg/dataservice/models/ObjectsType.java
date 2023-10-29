0171e291fe11eeab1339248db289c66e980639fe
Nabokov79 <nabokovsg@yandex.ru>
2023-10-29 19:05:06 +0300

    add fields: dataTemplates, repairMethods, documentations, defects, elements, norms

--- data-service/src/main/java/ru/nabokovsg/dataservice/models/ObjectsType.java
+++ data-service/src/main/java/ru/nabokovsg/dataservice/models/ObjectsType.java
@@ -1,8 +1,8 @@
 package ru.nabokovsg.dataservice.models;
 
 import lombok.*;
-
 import javax.persistence.*;
+import java.util.List;
 import java.util.Objects;
 
 @Setter
@@ -18,17 +18,67 @@ public class ObjectsType {
     private long id;
     @Column(name = "object_name")
     private String objectName;
+    @Column(name = "model")
+    private String model;
     @Column(name = "volume")
     private Integer volume;
     @Column(name = "orientation")
     private String orientation;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "object_types_object_passport_data_templates",
+            joinColumns = {@JoinColumn(name = "object_type_id")},
+            inverseJoinColumns = {@JoinColumn(name = "passport_data_template_id")})
+    @ToString.Exclude
+    private List<ObjectPassportDataTemplate> dataTemplates;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "object_types_repair_methods",
+            joinColumns = {@JoinColumn(name = "object_type_id")},
+            inverseJoinColumns = {@JoinColumn(name = "repair_method_id")})
+    @ToString.Exclude
+    private List<RepairMethod> repairMethods;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "object_types_documentations",
+            joinColumns = {@JoinColumn(name = "object_type_id")},
+            inverseJoinColumns = {@JoinColumn(name = "document_id")})
+    @ToString.Exclude
+    private List<Documentation> documentations;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "object_types_defects",
+            joinColumns = {@JoinColumn(name = "object_type_id")},
+            inverseJoinColumns = {@JoinColumn(name = "defect_id")})
+    @ToString.Exclude
+    private List<Defect> defects;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "object_types_elements",
+            joinColumns = {@JoinColumn(name = "object_type_id")},
+            inverseJoinColumns = {@JoinColumn(name = "element_id")})
+    @ToString.Exclude
+    private List<Element> elements;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "object_types_norms",
+            joinColumns = {@JoinColumn(name = "object_type_id")},
+            inverseJoinColumns = {@JoinColumn(name = "norm_id")})
+    @ToString.Exclude
+    private List<Norm> norms;
 
     @Override
     public String toString() {
         return "ObjectsType{" +
                 "id=" + id +
                 ", objectName='" + objectName + '\'' +
+                ", volume=" + volume +
                 ", orientation='" + orientation + '\'' +
+                ", dataTemplates=" + dataTemplates +
+                ", repairMethods=" + repairMethods +
+                ", documentations=" + documentations +
+                ", defects=" + defects +
+                ", elements=" + elements +
                 '}';
     }
 
@@ -39,11 +89,17 @@ public class ObjectsType {
         ObjectsType that = (ObjectsType) o;
         return id == that.id && Objects.equals(objectName, that.objectName)
                              && Objects.equals(volume, that.volume)
-                             && Objects.equals(orientation, that.orientation);
-                }
+                             && Objects.equals(orientation, that.orientation)
+                             && Objects.equals(dataTemplates, that.dataTemplates)
+                             && Objects.equals(repairMethods, that.repairMethods)
+                             && Objects.equals(documentations, that.documentations)
+                             && Objects.equals(defects, that.defects)
+                             && Objects.equals(elements, that.elements);
+    }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, objectName, volume, orientation);
+        return Objects.hash(id, objectName, volume, orientation, dataTemplates
+                              , repairMethods, documentations, defects, elements);
     }
 }
