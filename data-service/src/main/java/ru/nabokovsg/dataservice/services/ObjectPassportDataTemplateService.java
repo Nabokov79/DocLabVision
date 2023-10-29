44d8f67527b590b9c5746c44c176b588e2eb1418
Nabokov79 <nabokovsg@yandex.ru>
2023-10-29 19:03:35 +0300

    no changes

--- data-service/src/main/java/ru/nabokovsg/dataservice/services/ObjectPassportDataTemplateService.java
+++ data-service/src/main/java/ru/nabokovsg/dataservice/services/ObjectPassportDataTemplateService.java
@@ -3,11 +3,13 @@ package ru.nabokovsg.dataservice.services;
 import ru.nabokovsg.dataservice.dto.objectPassportDataTemplate.NewObjectPassportDataTemplateDto;
 import ru.nabokovsg.dataservice.dto.objectPassportDataTemplate.ObjectPassportDataTemplateDto;
 import ru.nabokovsg.dataservice.dto.objectPassportDataTemplate.UpdateObjectPassportDataTemplateDto;
+import ru.nabokovsg.dataservice.dto.objectsType.ObjectsTypePassportDataTemplateDto;
+
 import java.util.List;
 
 public interface ObjectPassportDataTemplateService {
 
-    List<ObjectPassportDataTemplateDto> save(List<Long> objectsTypeId
+    List<ObjectsTypePassportDataTemplateDto> save(List<Long> objectsTypeId
                                             , List<NewObjectPassportDataTemplateDto> templatesDto);
 
     List<ObjectPassportDataTemplateDto> update(List<UpdateObjectPassportDataTemplateDto> templatesDto);
