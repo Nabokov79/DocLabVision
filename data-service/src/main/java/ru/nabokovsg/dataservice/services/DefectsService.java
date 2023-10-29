6d2ca7ad0787253f330222645bbeff2900301faa
Nabokov79 <nabokovsg@yandex.ru>
2023-10-29 18:53:40 +0300

    without changes

--- data-service/src/main/java/ru/nabokovsg/dataservice/services/DefectsService.java
+++ data-service/src/main/java/ru/nabokovsg/dataservice/services/DefectsService.java
@@ -3,11 +3,13 @@ package ru.nabokovsg.dataservice.services;
 import ru.nabokovsg.dataservice.dto.defect.DefectDto;
 import ru.nabokovsg.dataservice.dto.defect.NewDefectDto;
 import ru.nabokovsg.dataservice.dto.defect.UpdateDefectDto;
+import ru.nabokovsg.dataservice.dto.objectsType.ObjectsTypeDefectDto;
+
 import java.util.List;
 
 public interface DefectsService {
 
-    List<DefectDto> save(List<Long> objectsTypeId, List<NewDefectDto> defects);
+    List<ObjectsTypeDefectDto> save(List<Long> objectsTypeId, List<NewDefectDto> defects);
 
     List<DefectDto> update(List<UpdateDefectDto> defects);
 }
