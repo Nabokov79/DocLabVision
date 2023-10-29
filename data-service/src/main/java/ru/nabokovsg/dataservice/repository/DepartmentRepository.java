7e20a04c4bf145045d892396f53551ac7150438b
Nabokov79 <nabokovsg@yandex.ru>
2023-10-29 18:54:31 +0300

    add method findByDepartment

--- data-service/src/main/java/ru/nabokovsg/dataservice/repository/DepartmentRepository.java
+++ data-service/src/main/java/ru/nabokovsg/dataservice/repository/DepartmentRepository.java
@@ -9,4 +9,6 @@ import java.util.Set;
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
 
     Set<Department> findByBranch(Branch branch);
+
+    Department findByDepartment(String department);
 }
