f5db38fef184104862e871d901defce59993832e
Nabokov79 <nabokovsg@yandex.ru>
2023-10-29 18:57:57 +0300

    no changes

--- data-service/src/main/java/ru/nabokovsg/dataservice/services/ElementService.java
+++ data-service/src/main/java/ru/nabokovsg/dataservice/services/ElementService.java
@@ -3,16 +3,15 @@ package ru.nabokovsg.dataservice.services;
 import ru.nabokovsg.dataservice.dto.element.ElementDto;
 import ru.nabokovsg.dataservice.dto.element.NewElementDto;
 import ru.nabokovsg.dataservice.dto.element.UpdateElementDto;
-import ru.nabokovsg.dataservice.dto.objectsType.ObjectsTypeDto;
+import ru.nabokovsg.dataservice.dto.objectsType.ObjectsTypeElementsDto;
 
 import java.util.List;
 
-
 public interface ElementService {
 
-    List<ElementDto> save(List<Long> objectsTypeId, List<NewElementDto> elementsDto);
+    List<ObjectsTypeElementsDto> save(List<Long> objectsTypeId, List<NewElementDto> elementsDto);
 
     List<ElementDto> update(List<UpdateElementDto> elementsDto);
 
-    ObjectsTypeDto getAll(Long objectsTypeId);
+    ObjectsTypeElementsDto getAll(Long objectsTypeId);
 }
