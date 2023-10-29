d91ba77ead08d2db965138f280eba7ab7f692384
Nabokov79 <nabokovsg@yandex.ru>
2023-10-29 18:56:24 +0300

    no changes

--- data-service/src/main/java/ru/nabokovsg/dataservice/services/DocumentationService.java
+++ data-service/src/main/java/ru/nabokovsg/dataservice/services/DocumentationService.java
@@ -3,11 +3,13 @@ package ru.nabokovsg.dataservice.services;
 import ru.nabokovsg.dataservice.dto.documentation.DocumentationDto;
 import ru.nabokovsg.dataservice.dto.documentation.NewDocumentationDto;
 import ru.nabokovsg.dataservice.dto.documentation.UpdateDocumentationDto;
+import ru.nabokovsg.dataservice.dto.objectsType.ObjectsTypeDocumentationDto;
+
 import java.util.List;
 
 public interface DocumentationService {
 
-    List<DocumentationDto> save(List<Long> objectsTypeId, List<NewDocumentationDto> documentationsDto);
+    List<ObjectsTypeDocumentationDto> save(List<Long> objectsTypeId, List<NewDocumentationDto> documentationsDto);
 
     List<DocumentationDto> update(List<UpdateDocumentationDto> documentationsDto);
 }
