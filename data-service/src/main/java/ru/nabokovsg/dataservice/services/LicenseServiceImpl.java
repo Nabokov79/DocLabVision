a98dce3b3a10d599a28df646d3971edb21fafb74
NabokovSG <nabokovsg@yandex.ru>
2023-10-30 16:42:21 +0300

    update method save

--- data-service/src/main/java/ru/nabokovsg/dataservice/services/LicenseServiceImpl.java
+++ data-service/src/main/java/ru/nabokovsg/dataservice/services/LicenseServiceImpl.java
@@ -2,19 +2,17 @@ package ru.nabokovsg.dataservice.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import ru.nabokovsg.dataservice.dto.ObjectsIds;
 import ru.nabokovsg.dataservice.dto.license.LicenseDto;
 import ru.nabokovsg.dataservice.dto.license.NewLicenseDto;
 import ru.nabokovsg.dataservice.dto.license.UpdateLicenseDto;
+import ru.nabokovsg.dataservice.enums.DivisionType;
+import ru.nabokovsg.dataservice.exceptions.BadRequestException;
 import ru.nabokovsg.dataservice.exceptions.NotFoundException;
 import ru.nabokovsg.dataservice.mappers.LicenseMapper;
-import ru.nabokovsg.dataservice.enums.BuilderType;
-import ru.nabokovsg.dataservice.models.DataBuilder;
 import ru.nabokovsg.dataservice.models.Licenses;
 import ru.nabokovsg.dataservice.repository.LicenseRepository;
-import ru.nabokovsg.dataservice.services.builder.DataFactory;
+
 import java.util.List;
-import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -22,22 +20,36 @@ public class LicenseServiceImpl implements LicenseService {
 
     private final LicenseRepository repository;
     private final LicenseMapper mapper;
-    private final DataFactory factory;
-
+    private final OrganizationService organizationService;
+    private final BranchService branchService;
+    private final DepartmentService departmentService;
 
     @Override
     public LicenseDto save(NewLicenseDto licenseDto) {
-        return mapper.mapToLicenseDto(Objects.requireNonNullElseGet(
-                repository.findByLicenseNumber(licenseDto.getLicenseNumber())
-                                                 , () -> repository.save(set(mapper.mapToNewLicense(licenseDto)
-                                                 , mapper.mapFromNewLicense(licenseDto)))));
+        Licenses license = mapper.mapToNewLicense(licenseDto);
+        license.setIssuedLicense(organizationService.getById(licenseDto.getIssuedLicenseId()));
+        try {
+            license = repository.save(license);
+        } catch (RuntimeException e) {
+            return mapper.mapToLicenseDto(license);
+        }
+        DivisionType type = DivisionType.from(licenseDto.getDivisionType())
+                .orElseThrow(() -> new BadRequestException(String.format("Unknown data format license=%s"
+                                                                                    , licenseDto.getDivisionType())));
+        switch (type) {
+            case ORGANIZATION -> organizationService.addLicense(licenseDto.getDivisionId(), license);
+            case BRANCH -> branchService.addLicense(licenseDto.getDivisionId(), license);
+            case DEPARTMENT -> departmentService.addLicense(licenseDto.getDivisionId(), license);
+            default ->  throw new BadRequestException(
+                               String.format("Data format is not supported, license=%s", licenseDto.getDivisionType()));
+        }
+        return mapper.mapToLicenseDto(license);
     }
 
     @Override
     public LicenseDto update(UpdateLicenseDto licenseDto) {
         if (repository.existsById(licenseDto.getId())) {
-            return mapper.mapToLicenseDto(repository.save(set(mapper.mapToUpdateLicense(licenseDto)
-                    , mapper.mapFromUpdateLicense(licenseDto))));
+            return mapper.mapToLicenseDto(repository.save(mapper.mapToUpdateLicense(licenseDto)));
         }
         throw new NotFoundException(String.format("license with id=%s not found for update", licenseDto.getId()));
     }
@@ -51,20 +63,5 @@ public class LicenseServiceImpl implements LicenseService {
     @Override
     public List<LicenseDto> getAll() {
         return mapper.mapToLicensesDto(repository.findAll());
-    }
-
-    private Licenses set(Licenses license, ObjectsIds ids) {
-        DataBuilder builder = factory.getBuilder(List.of(ids), BuilderType.LICENSE);
-        if (ids.getOrganizationId() != null) {
-            license.setOrganization(builder.getOrganizations().get(ids.getOrganizationId()));
-        }
-        license.setIssuedLicense(builder.getOrganizations().get(ids.getIssuedLicenseId()));
-        if(ids.getBranchId() != null) {
-            license.setBranch(builder.getBranches().get(ids.getBranchId()));
-        }
-        if (ids.getDepartmentId() != null) {
-            license.setDepartment(builder.getDepartments().get(ids.getDepartmentId()));
-        }
-        return license;
     }
 }
